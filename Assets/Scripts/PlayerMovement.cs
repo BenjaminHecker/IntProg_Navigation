52e4e9ceced10cf1f0377361ff69a9c33d8d6cc7
BenjaminHecker <benhecker7@gmail.com>
2023-02-24 23:29:44 -0500

    Added animator blend tree

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,32 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    [SerializeField] private float speed = 3f;
-    [SerializeField] private LayerMask ground;
-
-    private NavMeshAgent nav;
-
-    private void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        nav = GetComponent<NavMeshAgent>();
-        nav.speed = speed;
-    }
+        [SerializeField] private float speed = 3f;
+        [SerializeField] private LayerMask ground;
 
-    private void Update()
-    {
-        if (Input.GetMouseButtonUp(0))
+        private NavMeshAgent nav;
+        private Animator anim;
+
+        private Vector3 prevPos;
+
+        private void Start()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            nav = GetComponent<NavMeshAgent>();
+            nav.speed = speed;
 
-            if (Physics.Raycast(ray, out RaycastHit hit, 100f, ground.value))
+            anim = GetComponent<Animator>();
+
+            prevPos = transform.position;
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonUp(0))
             {
-                nav.SetDestination(hit.point);
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-                Debug.Log(hit.point);
-                Debug.DrawRay(hit.point, Vector3.up, Color.green, 1f);
+                if (Physics.Raycast(ray, out RaycastHit hit, 100f, ground.value))
+                {
+                    nav.SetDestination(hit.point);
+
+                    Debug.DrawRay(hit.point, Vector3.up, Color.green, 1f);
+                }
             }
+
+            float move = (transform.position - prevPos).magnitude / Time.deltaTime;
+            prevPos = transform.position;
+
+            anim.SetFloat("speed", Mathf.InverseLerp(0, speed, move));
         }
     }
 }
